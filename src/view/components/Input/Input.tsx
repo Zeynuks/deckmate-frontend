9ba9207119b862d43b73604f968f93a2a6945031
zeynuks <leznin2000@ya.ru>
2024-09-29 13:11:12 +0300

    Добавил Input

---
+++ src/view/components/Input/Input.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import styles from './Input.module.css';
+
+type InputProps = {
+    value: string;
+    onChange: (value: string) => void;
+    placeholder?: string;
+};
+
+export const Input: React.FC<InputProps> = ({ value, onChange, placeholder }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.code === 'Enter') {
+            inputRef.current?.blur();
+        }
+    };
+
+    return (
+        <input
+            ref={inputRef}
+            type="text"
+            value={value}
+            onChange={e => onChange(e.target.value)}
+            placeholder={placeholder}
+            onKeyDown={handleKeyDown}
+            className={styles.input}
+            tabIndex={0}
+        />
+    );
+};
