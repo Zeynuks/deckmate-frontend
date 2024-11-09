1a678696b2739d306957136a553cda0c0f857bcc
zeynuks <leznin2000@ya.ru>
2024-11-09 19:24:09 +0300

    Изменил прицип работы removeSlide

--- src/store/functions/removeSlide.ts
+++ src/store/functions/removeSlide.ts
@@ -1,26 +1,32 @@
-import {Editor} from '../types.ts';
+import { Editor } from '../types.ts';
 
 export function removeSlide(editor: Editor): Editor {
-    if(!editor) {
+    if (!editor || !editor.presentation.slides) {
         return editor;
     }
+
+    const slides = editor.presentation.slides ?? [];
     const removedSlideId = editor.selected.slide;
-    const slides = editor.presentation.slides || [];
-    const removeSlideIndex = slides.findIndex(slide => slide.id === removedSlideId);
-    const newSlides = editor.presentation.slides.filter(slide => slide.id !== removedSlideId);
-    let newSelectedSlideId = null;
-    if (newSlides.length > 0) {
-        const index = Math.min(removeSlideIndex, newSlides.length - 1);
-        newSelectedSlideId = newSlides[index].id;
+    const filteredSlides = slides.filter(slide => slide.id !== removedSlideId);
+
+    if (filteredSlides.length === slides.length) {
+        return editor;
     }
-    return <Editor>{
+
+    const removedSlideIndex = slides.findIndex(slide => slide.id === removedSlideId);
+    const nextSelectedSlideId = filteredSlides.length > 0
+        ? filteredSlides[Math.min(removedSlideIndex, filteredSlides.length - 1)].id
+        : undefined;
+
+    return {
+        ...editor,
         presentation: {
             ...editor.presentation,
-            slides: newSlides
+            slides: filteredSlides,
         },
         selected: {
             ...editor.selected,
-            slide: newSelectedSlideId,
-        }
+            slide: nextSelectedSlideId,
+        },
     };
 }
